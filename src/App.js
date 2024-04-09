253f01974b8b1ab8f8c39b031b6e0562b0d40992
jahnavi-sk <jahnavi31blr@gmail.com>
2024-04-10 00:38:05 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          <h1 className='text-3xl font-bold flex justify-between items-center'>Weather</h1>
+        </div>
+      </div>
     </div>
   );
 }
