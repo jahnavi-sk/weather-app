9a4c63d0e1d6f29d996862fea53ff91f41aba5d9
jahnavi-sk <jahnavi31blr@gmail.com>
2024-04-10 18:04:36 +0530

    phone responsive

--- src/App.js
+++ src/App.js
@@ -19,33 +19,42 @@ function App() {
       </div>
 
 
-    <div className='pl-44 flex'>
+    <div className='pl-10 pr-10 md:pl-44 md:flex'>
 
       <div id="1st two" className='flex-1'>
 
         <div>
-          <div className="card hover:shadow-lg w-5/6 h-96">
+          <div className="card hover:shadow-lg md:w-5/6 md:h-96">
             <div>
             <img src="images/newyorkimg.jpg" alt="new york" className="w-full h-80" ></img>
             {/* <img src="images/newyorkimg.jpg" alt="new york" className="block sm:hidden w-full h-48 object-cover" ></img> */}
             </div>
             <div>
-            <span className="font-bold text-lg">New York</span>
-              <span className="block text-gray-500 text-md">USA</span>
+              <div className='flex justify-center md:justify-start'>
+                <span className="font-bold text-lg">New York</span>
+              </div>
+              <div className='flex justify-center md:justify-start'>
+                <span className="block text-gray-500 text-md">USA</span>
+              </div>
             </div>
             {/* card */}
           </div>
         </div>
 
         <div>
-          <div className="card hover:shadow-lg w-5/6 h-96">
+          <div className="card hover:shadow-lg md:w-5/6 md:h-96">
             <div>
             <img src="images/london.jpg" alt="new york" className="w-full h-80" ></img>
             {/* <img src="images/newyorkimg.jpg" alt="new york" className="block sm:hidden w-full h-48 object-cover" ></img> */}
             </div>
             <div>
-            <span className="font-bold text-lg">London</span>
-              <span className="block text-gray-500 text-md">UK</span>
+              <div className='flex justify-center md:justify-start'>
+                <span className="font-bold text-lg">London</span>
+              </div>
+              <div className='flex justify-center md:justify-start'>
+                <span className="block text-gray-500 text-md">UK</span>
+              </div>
+              
             </div>
             {/* card */}
           </div>
@@ -56,28 +65,41 @@ function App() {
 
       <div id="2nd two" className='flex-1'>
         <div>
-          <div className="card hover:shadow-lg w-5/6 h-96">
+          <div className="card hover:shadow-lg md:w-5/6 md:h-96">
             <div>
             <img src="images/delhi.jpg" alt="new york" className="w-full h-80" ></img>
             {/* <img src="images/newyorkimg.jpg" alt="new york" className="block sm:hidden w-full h-48 object-cover" ></img> */}
             </div>
+
             <div>
-            <span className="font-bold text-lg">Delhi</span>
-              <span className="block text-gray-500 text-md">India</span>
+              <div className='flex justify-center md:justify-start'>
+                <span className="font-bold text-lg">Delhi</span>
+              </div>
+
+              <div className='flex justify-center md:justify-start'>
+                <span className="block text-gray-500 text-md">India</span>
+              </div>
             </div>
+            
             {/* card */}
           </div>
         </div>
 
         <div>
-          <div className="card hover:shadow-lg w-5/6 h-96">
+          <div className="card hover:shadow-lg md:w-5/6 md:h-96">
             <div>
             <img src="images/seoul.jpg" alt="new york" className="w-full h-80" ></img>
             {/* <img src="images/newyorkimg.jpg" alt="new york" className="block sm:hidden w-full h-48 object-cover" ></img> */}
             </div>
+
+
             <div>
-            <span className="font-bold text-lg">Seoul</span>
-              <span className="block text-gray-500 text-md">South Korea</span>
+              <div className='flex justify-center md:justify-start'>
+                <span className="font-bold text-lg">Seoul</span>
+              </div>
+              <div className='flex justify-center md:justify-start'>
+                <span className="block text-gray-500 text-md">South Korea</span>
+              </div>
             </div>
             {/* card */}
           </div>
